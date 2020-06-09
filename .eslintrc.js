bae0f678e2cc44cf64624676897242465dfac8d8
Marcio Augusto Guimarães <marcio.guimaraes@edge.ufal.br>
2020-06-09 13:20:31 -0300

    add prettier workflows.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,20 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint'
-    ],
+    plugins: ['@typescript-eslint'],
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
         'plugin:import/errors',
         'plugin:import/warnings',
         'plugin:import/typescript',
-        'prettier'
-    ]
+        'prettier',
+    ],
+    rules: {
+        'prettier/prettier': 'error',
+    },
+    env: {
+        node: true,
+    },
 };
